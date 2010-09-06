becdc83bc911f481d332cc4219046aab4f0b9289
Leon Winter <lwi@ring0.de>
2010-09-06 23:04:36 +0200

    level6

---
+++ level6.c
@@ -0,0 +1,35 @@
+/* by lwi */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MIN(a, b) (a > b ? b : a)
+#define OFFSET 40
+
+int main(int argc, char **argv, char **envp) {
+	{	/* force slot for system() in binary, because address of
+			system() in libc contains NUL char, making it
+			impossible to use it in an exploit */
+		int (*f)(const char *cmd) = &system;
+		f = &puts;
+	}
+
+	static char str[] = "hello.\n";
+	char *p = str, *a;
+
+	if(argc != 2 || strlen(argv[0]) != 7 || strlen(argv[1]) > (OFFSET + 12))
+		return -1;
+	/* clear the enviroment to make it harder */
+	for(; *envp; envp++)
+		bzero(*envp, strlen(*envp));
+	
+	a = argv[0];
+	while(*(p++) ^= *(a++));
+	bzero(argv[0], 8);
+
+	size_t len = strlen(argv[1]);
+	bzero(argv[1], MIN(len, OFFSET));
+
+	char buf[8], i = 'a';
+	memcpy(buf, argv[1], len); /* oops */
+}
